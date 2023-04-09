5dea85ecf8632ebea95b07a621169786d4faec07
burondt <denisalexandrovich@inbox.ru>
2023-04-10 01:51:34 +0300

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__description">© 2022-2023 Mesto Russia</p>
-    </footer>
-  );
-}
-
-export default Footer;
